62b81ef6d15b594c859aa7dcae2bfc9b35067ca2
Dementors71 <252518865@qq.com>
2018-07-28 19:55:37 +0800

    Add new funciton & change some info
    1. Add object detection based on "ssd_mobilenet_v1_coco".
    2. Remove some personal information in some codes.

---
+++ advanced_model/obj_detect.py
@@ -0,0 +1,162 @@
+# Object Detection (Receive Image & Send Result)
+# 2018-07-28
+# Copyright (c) Cen XIN
+
+from __future__ import print_function
+import paho.mqtt.client as mqtt
+
+import numpy as np
+import os
+import six.moves.urllib as urllib
+import sys
+import tensorflow as tf
+import time
+
+from collections import defaultdict
+from io import StringIO
+from PIL import Image
+
+# This is needed since the notebook is stored in the object_detection folder.
+sys.path.append("..")
+from object_detection.utils import ops as utils_ops
+from utils import label_map_util
+from utils import visualization_utils as vis_util
+
+MODEL_NAME = 'ssd_mobilenet_v1_coco_2018_01_28'
+MODEL_FILE = MODEL_NAME + '.tar.gz'
+
+# Path to frozen detection graph. This is the actual model that is used for the object detection.
+PATH_TO_CKPT = MODEL_NAME + '/frozen_inference_graph.pb'
+
+# List of the strings that is used to add correct label for each box.
+PATH_TO_LABELS = os.path.join('data', 'mscoco_label_map.pbtxt')
+
+NUM_CLASSES = 90
+
+
+def load_image_into_numpy_array(image):
+  (im_width, im_height) = image.size
+  return np.array(image.getdata()).reshape(
+      (im_height, im_width, 3)).astype(np.uint8)
+
+IMAGE_PATH = 'tmp/image.jpg'
+
+
+def run_inference_for_single_image(image, graph):
+  with graph.as_default():
+    with tf.Session() as sess:
+      # Get handles to input and output tensors
+      ops = tf.get_default_graph().get_operations()
+      all_tensor_names = {output.name for op in ops for output in op.outputs}
+      tensor_dict = {}
+      for key in [
+          'num_detections', 'detection_boxes', 'detection_scores',
+          'detection_classes', 'detection_masks'
+      ]:
+        tensor_name = key + ':0'
+        if tensor_name in all_tensor_names:
+          tensor_dict[key] = tf.get_default_graph().get_tensor_by_name(
+              tensor_name)
+      if 'detection_masks' in tensor_dict:
+        # The following processing is only for single image
+        detection_boxes = tf.squeeze(tensor_dict['detection_boxes'], [0])
+        detection_masks = tf.squeeze(tensor_dict['detection_masks'], [0])
+        # Reframe is required to translate mask from box coordinates to image coordinates and fit the image size.
+        real_num_detection = tf.cast(tensor_dict['num_detections'][0], tf.int32)
+        detection_boxes = tf.slice(detection_boxes, [0, 0], [real_num_detection, -1])
+        detection_masks = tf.slice(detection_masks, [0, 0, 0], [real_num_detection, -1, -1])
+        detection_masks_reframed = utils_ops.reframe_box_masks_to_image_masks(
+            detection_masks, detection_boxes, image.shape[0], image.shape[1])
+        detection_masks_reframed = tf.cast(
+            tf.greater(detection_masks_reframed, 0.5), tf.uint8)
+        # Follow the convention by adding back the batch dimension
+        tensor_dict['detection_masks'] = tf.expand_dims(
+            detection_masks_reframed, 0)
+      image_tensor = tf.get_default_graph().get_tensor_by_name('image_tensor:0')
+
+      # Run inference
+      output_dict = sess.run(tensor_dict,
+                             feed_dict={image_tensor: np.expand_dims(image, 0)})
+
+      # all outputs are float32 numpy arrays, so convert types as appropriate
+      output_dict['num_detections'] = int(output_dict['num_detections'][0])
+      output_dict['detection_classes'] = output_dict[
+          'detection_classes'][0].astype(np.uint8)
+      output_dict['detection_boxes'] = output_dict['detection_boxes'][0]
+      output_dict['detection_scores'] = output_dict['detection_scores'][0]
+      if 'detection_masks' in output_dict:
+        output_dict['detection_masks'] = output_dict['detection_masks'][0]
+  return output_dict
+
+
+def on_connect(client, userdata, flags, rc):
+  print('Connected with result code %d.' % rc)
+  client.subscribe('SWS3009TEAM6')
+
+
+def on_message(client, userdata, msg):
+  global model
+  print('Received message. Writting to %s.' % IMAGE_PATH)
+  img = msg.payload
+  time.sleep(2)
+  with open(IMAGE_PATH, 'wb') as f:
+    f.write(img)
+
+  image = Image.open(IMAGE_PATH)
+  image_np = load_image_into_numpy_array(image)
+  image_np_expanded = np.expand_dims(image_np, axis=0)
+  output_dict = run_inference_for_single_image(image_np, detection_graph)
+  vis_util.visualize_boxes_and_labels_on_image_array(
+      image_np,
+      output_dict['detection_boxes'],
+      output_dict['detection_classes'],
+      output_dict['detection_scores'],
+      category_index,
+      instance_masks=output_dict.get('detection_masks'),
+      use_normalized_coordinates=True,
+      line_thickness=8)
+  obj_num = output_dict['num_detections']
+  person_num = 0
+  for i in range(obj_num):
+    obj_id = category_index[output_dict['detection_classes'][i]]['id']
+    if obj_id == 1:
+      person_num = person_num + 1
+  print('Person number = %d' % person_num)
+  client.publish('SWS3009TEAM6', person_num)
+
+
+def setup(host_name):
+  global detection_graph
+  global label_map
+  global categories
+  global category_index
+  detection_graph = tf.Graph()
+  with detection_graph.as_default():
+    od_graph_def = tf.GraphDef()
+    with tf.gfile.GFile(PATH_TO_CKPT, 'rb') as fid:
+      serialized_graph = fid.read()
+      od_graph_def.ParseFromString(serialized_graph)
+      tf.import_graph_def(od_graph_def, name='')
+
+  label_map = label_map_util.load_labelmap(PATH_TO_LABELS)
+  categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=NUM_CLASSES, use_display_name=True)
+  category_index = label_map_util.create_category_index(categories)
+
+  client = mqtt.Client()
+  client.on_connect = on_connect
+  client.on_message = on_message
+  client.username_pw_set(username='********', password='********')
+  client.connect(host_name)
+  client.loop_start()
+
+
+def main():
+  setup('********')
+  while True:
+    pass
+
+
+if __name__ == '__main__':
+  main()
+
+
